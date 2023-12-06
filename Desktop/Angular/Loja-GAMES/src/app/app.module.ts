0681301df78064d1e752e92874eaef8bdc86fbe8
LuanBarbosaD <luan.barbosa410@outlook.com>
2023-12-06 19:09:33 -0300

    Angular

---
+++ Desktop/Angular/Loja-GAMES/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import {MatButtonModule} from '@angular/material/button';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Componente1Component } from './componente1/componente1.component';
+import { Componente2Component } from './componente2/componente2.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Componente1Component,
+    Componente2Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
